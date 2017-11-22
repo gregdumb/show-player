882f4546b68a1e676a0b7c8737677c3f3a84dcf8
Greg Brisebois <gregoryjjb@gmail.com>
2017-11-22 00:24:10 +0000

    Fixed playshow being wrong

--- playshow.py
+++ playshow.py
@@ -1,6 +1,7 @@
 import time
 import sys
 from pygame import mixer
+import RPi.GPIO as GPIO
 
 if(len(sys.argv) < 3):
 	sys.exit()
@@ -13,15 +14,42 @@ print(sys.argv[1])
 event_raw = []
 
 with open (SHOW_PATH, 'r') as show:
-	event_raw = show.readlines()
+    event_raw = [line.rstrip() for line in show]
 
 next_frame = 0
 
+#pin = 4
+
+ON = GPIO.HIGH
+OFF = GPIO.LOW
+
+pins = [4, 17, 27, 22]
+
+GPIO.setmode(GPIO.BCM)
+
+for pin in pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, OFF)
+
+#led = LED(4)
+
+def execute_pinout(states):
+    print(str(states))
+    for i in range(0, 3): # REPLACE 3 with range(states)
+        if states[i] == '0':
+            #led.off()
+            #print('Turning off')
+            GPIO.output(pins[i], OFF)
+        else:
+            #led.on()
+            #print('Turning on')
+            GPIO.output(pins[i], ON)
+
+# Play music
+mixer.pre_init(44100, -16, 2, 2048)
 mixer.init()
 print("loading music")
 mixer.music.load(SONG_PATH)
-
-time.sleep(2)
 
 mixer.music.play()
 
@@ -32,7 +60,7 @@ while True:
 	PREV_TIME = RUN_TIME
 	RUN_TIME = time.time() - START_TIME
 	DELTA_TIME = RUN_TIME - PREV_TIME
-	#print("DeltaTime: " + str(DELTA_TIME))
+	#print("RunTime: " + str(RUN_TIME))
 	
 	frame = event_raw[next_frame]
 	elements = frame.split(',')
@@ -44,7 +72,8 @@ while True:
 		event_string = event_string + str(elements[i])
 	
 	if RUN_TIME >= next_time:
-		print("Event: " + event_string)
+		elements.pop(0)
+		execute_pinout(elements)
 		next_frame += 1
 		if next_frame == len(event_raw):
 			break
